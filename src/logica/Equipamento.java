accd2ad85ca7d6681ff05c619ffc70d8f6771a05
Elder Xavier <eldersxavier@gmail.com>
2014-11-09 08:57:31 -0200

    Projeto inicial

---
+++ src/logica/Equipamento.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author X-E
+ */
+public class Equipamento {
+    private String tag;
+    private double tempodisponivel;
+    private double tempoindisponivel;
+
+    /**
+     * @return the tag
+     */
+    public String getTag() {
+        return tag;
+    }
+
+    /**
+     * @param tag the tag to set
+     */
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    /**
+     * @return the tempodisponivel
+     */
+    public double getTempodisponivel() {
+        return tempodisponivel;
+    }
+
+    /**
+     * @param tempodisponivel the tempodisponivel to set
+     */
+    public void setTempodisponivel(double tempodisponivel) {
+        this.tempodisponivel = tempodisponivel;
+    }
+
+    /**
+     * @return the tempoindisponivel
+     */
+    public double getTempoindisponivel() {
+        return tempoindisponivel;
+    }
+
+    /**
+     * @param tempoindisponivel the tempoindisponivel to set
+     */
+    public void setTempoindisponivel(double tempoindisponivel) {
+        this.tempoindisponivel = tempoindisponivel;
+    }
+
+
+}
+
---
+++ src/matematicaii_atividade_06112014/MatematicaII_atividade_06112014.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matematicaii_atividade_06112014;
+
+import formularios.Formulario1;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
+/**
+ *
+ * @author X-E
+ */
+public class MatematicaII_atividade_06112014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario1 formulario = new Formulario1();            
+        //URL url = formulario.getClass().getResource("../imagens/logo.jpeg");            
+        //Image iconeTitulo = Toolkit.getDefaultToolkit().getImage(url);    
+        //formulario.setIconImage(iconeTitulo);          
+        formulario.setVisible(true);
+    }
+}
